827146bb444728fc315481ee163ba0fc79c62fba
Stefan Cvetanovski <cvetanovskistefan72@gmail.com>
2021-01-22 13:29:00 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BooksList } from './components/BooksList';
 import { Filters } from './components/Filters';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
+       <Header/>  
       <Filters/>
       <BooksList/>
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-div">
+                <ul>
+                    <li>Home</li>
+                    <li>Books</li>
+                    <li>About</li>
+                </ul>
+            </div>
+            
+        </div>
+    )
+}
