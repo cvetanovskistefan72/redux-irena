0d0d6198900759880eed77a693f86888abe24464
Irena Nojkova Angelova <irena_nojkova@yahoo.com>
2021-01-22 13:52:32 +0100

    Contact Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export const Header = () => {
-    return (
-        <div className="header">
-            <div className="header-div">
-                <ul>
-                    <li>Home</li>
-                    <li>Books</li>
-                    <li>About</li>
-                </ul>
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header-div">
+        <ul>
+          <li>Home</li>
+          <li>Books</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
